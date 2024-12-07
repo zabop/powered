aab620f9892f23a0bede00befaa551c2b95684f8
zabop <szabopal96@gmail.com>
2024-12-07 22:44:22 +0100

    circle map with full screen map works

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import MapWithFixedCircle from "./MapWithFixedCircle";
+import "./App.css"; // Importing the CSS file
+
+const App = () => {
+  return <MapWithFixedCircle />;
+};
+
+export default App;
