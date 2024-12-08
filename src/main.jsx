09321f36c14c02aaf7b3f46e6b118aec9e2c251d
zabop <szabopal96@gmail.com>
2024-12-08 15:49:46 +0100

    simplification

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
+import MapWithFixedCircle from "./MapWithFixedCircle";
+import "./main.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <MapWithFixedCircle />
   </StrictMode>
 );
